bcfe7e57667ef712570969074cad6a97433b66ce
Otubusin Ademuyiwa <53128146+tay4real@users.noreply.github.com>
2021-02-19 19:10:26 +0100

    Update index.js

--- src/services/albums/index.js
+++ src/services/albums/index.js
@@ -1,7 +1,8 @@
-const express = require("express")
-const axios = require("axios")
+const express = require("express");
+const axios = require("axios");
+const { authorize } = require("../auth/middlewares");
 
-const albumsRouter = express.Router()
+const albumsRouter = express.Router();
 
 const config = {
   method: "GET",
@@ -9,19 +10,19 @@ const config = {
     "x-rapidapi-key": "f8be2f0c65mshfad5043cb400d5dp12eb36jsn70f4e3e3750f",
     "x-rapidapi-host": "deezerdevs-deezer.p.rapidapi.com",
   },
-}
+};
 
 albumsRouter.get("/:id", async (req, res, next) => {
   try {
     let response = await axios.get(
       "https://deezerdevs-deezer.p.rapidapi.com/album/" + req.params.id,
       config
-    )
-    console.log(response.data)
-    res.send(response.data)
+    );
+    console.log(response.data);
+    res.send(response.data);
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
-})
+});
 
-module.exports = albumsRouter
+module.exports = albumsRouter;
