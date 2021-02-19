0182fabfaadc68dbb0e0188b6cfad00118e5a63a
Otubusin Ademuyiwa <53128146+tay4real@users.noreply.github.com>
2021-02-19 16:49:16 +0100

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,62 +1,65 @@
-const express = require("express")
-const cors = require("cors")
-const { join } = require("path")
-const listEndpoints = require("express-list-endpoints")
-const mongoose = require("mongoose")
-const passport = require("passport")
-const cookieParser = require("cookie-parser")
+const express = require("express");
+const cors = require("cors");
+const { join } = require("path");
+const listEndpoints = require("express-list-endpoints");
+const mongoose = require("mongoose");
+const passport = require("passport");
+const cookieParser = require("cookie-parser");
 
-const usersRouter = require("./services/users")
-const artistsRouter = require("./services/artists")
-const albumsRouter = require("./services/albums")
+const usersRouter = require("./services/users");
+const artistsRouter = require("./services/artists");
+const albumsRouter = require("./services/albums");
 
-const oauth = require("./services/auth/oauth")
+const oauth = require("./services/auth/oauth");
 
 const {
   notFoundHandler,
   forbiddenHandler,
   badRequestHandler,
   genericErrorHandler,
-} = require("./errorHandlers")
+} = require("./errorHandlers");
 
-const server = express()
+const server = express();
 
-const whitelist = ["http://localhost:3000"]
+const whiteList =
+  process.env.NODE_ENV === "production"
+    ? [process.env.FE_URL_PROD]
+    : [process.env.FE_URL_DEV];
+
 const corsOptions = {
   origin: (origin, callback) => {
     if (whitelist.indexOf(origin) !== -1 || !origin) {
-      callback(null, true)
+      callback(null, true);
     } else {
-      callback(new Error("Not allowed by CORS"))
+      callback(new Error("Not allowed by CORS"));
     }
   },
   credentials: true,
-}
+};
 
-server.use(cors(corsOptions))
-const port = process.env.PORT
+server.use(cors(corsOptions));
+const port = process.env.PORT;
 
-const staticFolderPath = join(__dirname, "../public")
-server.use(express.static(staticFolderPath))
-server.use(express.json())
-server.use(cookieParser())
-server.use(passport.initialize())
+const staticFolderPath = join(__dirname, "../public");
+server.use(express.static(staticFolderPath));
+server.use(express.json());
+server.use(cookieParser());
+server.use(passport.initialize());
 
-server.use("/users", usersRouter)
+server.use("/users", usersRouter);
 
-server.use("/artists", artistsRouter)
+server.use("/artists", artistsRouter);
 
-server.use("/albums", albumsRouter)
-
+server.use("/albums", albumsRouter);
 
 // ERROR HANDLERS MIDDLEWARES
 
-server.use(badRequestHandler)
-server.use(forbiddenHandler)
-server.use(notFoundHandler)
-server.use(genericErrorHandler)
+server.use(badRequestHandler);
+server.use(forbiddenHandler);
+server.use(notFoundHandler);
+server.use(genericErrorHandler);
 
-console.log(listEndpoints(server))
+console.log(listEndpoints(server));
 
 mongoose
   .connect(process.env.MONGO_CONNECTION, {
@@ -66,7 +69,7 @@ mongoose
   })
   .then(
     server.listen(port, () => {
-      console.log("Running on port", port)
+      console.log("Running on port", port);
     })
   )
-  .catch((err) => console.log(err))
+  .catch((err) => console.log(err));
